a1072cdb120d3bfc366881f0e99867ddcc9678f7
schulz <schulz>
2003-07-01 11:11:08 +0000

    Added files for frequency count vector indexing.

---
+++ CLAUSES/ccl_fcvindexing.h
@@ -0,0 +1,55 @@
+/*-----------------------------------------------------------------------
+
+File  : ccl_fcvindexing.h
+
+Author: Stephan Schulz
+
+Contents
+
+  Functions for handling frequency count vector indexing for clause
+  subsumption.
+
+  2003 by the author.
+  This code is released under the GNU General Public Licence.
+  See the file COPYING in the main CLIB directory for details.
+  Run "eprover -h" for contact information.
+
+Changes
+
+<1> Tue Jul  1 13:05:36 CEST 2003
+    New
+
+-----------------------------------------------------------------------*/
+
+#ifndef CCL_FCVINDEXING
+
+#define CCL_FCVINDEXING
+
+#include <clb_pdarrays.h>
+#include <ccl_clauses.h>
+
+
+/*---------------------------------------------------------------------*/
+/*                    Data type declarations                           */
+/*---------------------------------------------------------------------*/
+
+
+
+
+/*---------------------------------------------------------------------*/
+/*                Exported Functions and Variables                     */
+/*---------------------------------------------------------------------*/
+
+
+
+
+#endif
+
+/*---------------------------------------------------------------------*/
+/*                        End of File                                  */
+/*---------------------------------------------------------------------*/
+
+
+
+
+
