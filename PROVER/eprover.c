dd23d5448964db811d3d553c48e4813aa09e1bbb
schulz <schulz>
2003-08-29 22:25:13 +0000

    Changed TSTP output for Geoff. Upped version number.

--- PROVER/eprover.c
+++ PROVER/eprover.c
@@ -33,7 +33,7 @@ Changes
 /*                  Data types                                         */
 /*---------------------------------------------------------------------*/
 
-#define VERSION      "0.8dev005"
+#define VERSION      "0.8dev006"
 #define NAME         "eprover"
 
 #ifdef SAFELOGIC
