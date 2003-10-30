23efb5e3ce23cf95a525baca2bdf8bd8b055b1d9
schulz <schulz>
2003-10-30 20:36:59 +0000

    Added support for printing clause sets in TSTP. Changed default I/O format to TSTP. Upped development version number.

--- PROVER/eprover.c
+++ PROVER/eprover.c
@@ -33,7 +33,7 @@ Changes
 /*                  Data types                                         */
 /*---------------------------------------------------------------------*/
 
-#define VERSION      "0.8dev017"
+#define VERSION      "0.8dev018"
 #define NAME         "eprover"
 
 #ifdef SAFELOGIC
