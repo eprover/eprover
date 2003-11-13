dbb854c67cc8b5b41876d6c52e5bf78faf6b19f1
schulz <schulz>
2003-11-13 22:48:24 +0000

    Lots of code for handling input formulae.

---
+++ CLAUSES/ccl_formula_wrapper.c
@@ -0,0 +1,423 @@
+/*-----------------------------------------------------------------------
+
+File  : ccl_formula_wrapper.c
+
+Author: Stephan Schulz
+
+Contents
+
+  Wrapped formula code.
+
+  Copyright 1998-2003 by the author.
+  This code is released under the GNU General Public Licence.
+  See the file COPYING in the main CLIB directory for details.
+  Run "eprover -h" for contact information.
+
+Changes
+
+<1> Thu Nov 13 01:43:38 GMT 2003
+    New
+
+-----------------------------------------------------------------------*/
+
+#include "ccl_formula_wrapper.h"
+
+
+
+/*---------------------------------------------------------------------*/
+/*                        Global Variables                             */
+/*---------------------------------------------------------------------*/
+
+long FormulaIdentCounter = 0;
+
+/*---------------------------------------------------------------------*/
+/*                      Forward Declarations                           */
+/*---------------------------------------------------------------------*/
+
+
+/*---------------------------------------------------------------------*/
+/*                         Internal Functions                          */
+/*---------------------------------------------------------------------*/
+
+
+
+/*---------------------------------------------------------------------*/
+/*                         Exported Functions                          */
+/*---------------------------------------------------------------------*/
+
+/*-----------------------------------------------------------------------
+//
+// Function: DefaultWFormulaAlloc()
+//
+//   Allocate and return a wrapped formula cell with all values
+//   initialized to rational default values.
+//
+// Global Variables: -
+//
+// Side Effects    : Memory operations
+//
+/----------------------------------------------------------------------*/
+
+WFormula_p DefaultWFormulaAlloc()
+{
+   WFormula_p handle = WFormulaCellAlloc();
+   
+   handle->properties = FPIgnoreProps; 
+   handle->ident      = 0;
+   handle->ext_ident  = NULL;
+   handle->formula    = 0;
+   handle->set        = NULL;
+   handle->pred       = NULL;
+   handle->succ       = NULL;
+
+   return handle;
+}
+
+/*-----------------------------------------------------------------------
+//
+// Function: WFormulaAlloc()
+//
+//   Allocate a wrapped formula given the essential information. id
+//   will automagically be set to a new value.
+//
+// Global Variables: FormulaIdentCounter
+//
+// Side Effects    : Via DefaultWFormulaAlloc()
+//
+/----------------------------------------------------------------------*/
+
+WFormula_p WFormulaAlloc(Formula_p formula)
+{
+   WFormula_p handle = DefaultWFormulaAlloc();
+   
+   handle->formula = FormulaGetRef(formula);
+   handle->ident   = ++FormulaIdentCounter;
+   
+   return handle;
+}
+
+
+/*-----------------------------------------------------------------------
+//
+// Function: WFormulaFree()
+//
+//   Free a wrapped formula.
+//
+// Global Variables: -
+//
+// Side Effects    : Memory operations
+//
+/----------------------------------------------------------------------*/
+
+void WFormulaFree(WFormula_p form)
+{
+   assert(form);
+   assert(form->formula);
+   assert(!form->set);
+   assert(!form->pred);
+   assert(!form->succ);
+   
+   FormulaRelRef(form->formula);
+   FormulaFree(form->formula);
+   if(form->ext_ident)
+   {
+      FREE(form->ext_ident);
+   }
+   WFormulaCellFree(form);
+}
+
+
+/*-----------------------------------------------------------------------
+//
+// Function: WFormulaTPTPParse()
+//
+//   Parse a formula in TPTP format.
+//
+// Global Variables: -
+//
+// Side Effects    : Input
+//
+/----------------------------------------------------------------------*/
+
+WFormula_p WFormulaTPTPParse(Scanner_p in, TB_p terms)
+{
+   char*              name;
+   Formula_p          form;
+   WFormulaProperties type;
+   WFormula_p         handle;
+   
+   AcceptInpId(in, "input_formula");
+   AcceptInpTok(in, OpenBracket);
+   CheckInpTok(in, Name|PosInt);
+   name = DStrCopy(AktToken(in)->literal);
+   NextToken(in);
+   AcceptInpTok(in, Comma);
+   CheckInpId(in, "axiom|hypothesis|conjecture|lemma|unknown");
+   if(TestInpId(in, "conjecture"))
+   {
+      type = WPTypeConjecture;
+   }
+   else if(TestInpId(in, "hypothesis"))
+   {
+      type = WPTypeHypothesis;
+   }
+   else
+   {
+      type = WPTypeAxiom;
+   }
+   NextToken(in);
+   AcceptInpTok(in, Comma);
+   form = FormulaTPTPParse(in, terms); /* TSTP = TPTP! */
+   AcceptInpTok(in, CloseBracket);
+   AcceptInpTok(in, Fullstop);
+   handle = WFormulaAlloc(form);
+   handle->ext_ident = name;
+   FormulaSetType(handle, type);
+   FormulaSetProp(handle, WPInitial);
+
+   return handle;
+}
+
+/*-----------------------------------------------------------------------
+//
+// Function: FormulaTPTPPrint()
+//
+//   Print a formula in TPTP format.
+//
+// Global Variables: -
+//
+// Side Effects    : 
+//
+/----------------------------------------------------------------------*/
+
+void WFormulaTPTPPrint(FILE* out, WFormula_p form, bool fullterms)
+{
+   char *typename;
+
+   switch(FormulaQueryType(form))
+   {
+   case WPTypeAxiom:
+	 typename = "axiom";
+	 break;
+   case WPTypeHypothesis:
+	 typename = "hypothesis";
+	 break;      
+   case WPTypeConjecture:
+	 typename = "conjecture";
+	 break;
+   default:
+	 typename = "unknown";
+	 break;
+   }   
+   fprintf(out, "input_formula(f_%ld,%s,", form->ident, typename);
+   FormulaTPTPPrint(out, form->formula,fullterms);
+   fprintf(out,").");   
+}
+
+
+/*-----------------------------------------------------------------------
+//
+// Function: WFormulaTSTPParse()
+//
+//   Parse a formula in TSTP format.
+//
+// Global Variables: -
+//
+// Side Effects    : Input
+//
+/----------------------------------------------------------------------*/
+
+WFormula_p WFormulaTSTPParse(Scanner_p in, TB_p terms)
+{
+   char*              name;
+   Formula_p          form;
+   WFormulaProperties type = WPTypeAxiom;
+   WFormulaProperties initial = WPInitial;
+   WFormula_p         handle;
+   bool               check_type = true;
+      
+   AcceptInpId(in, "fof");
+   AcceptInpTok(in, OpenBracket);
+   CheckInpTok(in, Name);
+   name = DStrCopy(AktToken(in)->literal);
+   NextToken(in);
+   AcceptInpTok(in, Comma);
+   if(TestInpId(in, "initial|derived"))
+   {
+      if(TestInpId(in, "derived"))
+      {
+	 initial = WPIgnoreProps;
+      }
+      AcceptInpTok(in, Ident);
+      type = WPTypeAxiom;
+      check_type = false;
+      if(TestInpTok(in, Hyphen))
+      {
+	 AcceptInpTok(in, Hyphen);
+	 check_type = true;
+      }
+   }
+   if(check_type)
+   {
+      CheckInpId(in, 
+		 "axiom|definition|knowledge|assumption|"
+		 "hypothesis|conjecture|lemma|unknown");
+      if(TestInpId(in, "hypothesis"))
+      {
+	 type = WPTypeHypothesis;
+      }
+      else if(TestInpId(in, "conjecture"))
+      {
+	 type = WPTypeConjecture;
+      }
+      else
+      {
+	 type = WPTypeAxiom;
+      }
+   }   
+   NextToken(in);
+   AcceptInpTok(in, Comma);
+   form = FormulaTPTPParse(in, terms); /* TSTP = TPTP! */
+   AcceptInpTok(in, CloseBracket);
+   AcceptInpTok(in, Fullstop);
+   handle = WFormulaAlloc(form);
+   handle->ext_ident = name;
+   FormulaSetType(handle, type);
+   FormulaSetProp(handle, initial);
+
+   return handle;
+}
+
+
+/*-----------------------------------------------------------------------
+//
+// Function: FormulaTSTPPrint()
+//
+//   Print a formula in TSTP format. If !complete, leave of the
+//   trailing ")." for adding optional stuff.
+//
+// Global Variables: -
+//
+// Side Effects    : 
+//
+/----------------------------------------------------------------------*/
+
+void WFormulaTSTPPrint(FILE* out, WFormula_p form, bool fullterms,
+		       bool complete)
+{
+   char *typename, *initial="derived";
+
+   switch(FormulaQueryType(form))
+   {
+   case WPTypeAxiom:
+	 typename = "axiom";
+	 break;
+   case WPTypeHypothesis:
+	 typename = "hypothesis";
+	 break;      
+   case WPTypeConjecture:
+	 typename = "conjecture";
+	 break;
+   default:
+	 typename = "unknown";
+	 break;
+   }   
+   if(FormulaQueryProp(form, WPInitial))
+   {
+      initial = "initial";
+   }
+   fprintf(out, "fof(f_%ld,%s-%s,", form->ident, initial,typename);
+   FormulaTPTPPrint(out, form->formula,fullterms);
+   if(complete)
+   {
+      fprintf(out, ").");
+   }
+}
+
+
+/*-----------------------------------------------------------------------
+//
+// Function: FormulaSetAlloc()
+//
+//   Allocate and initialize a formula set.
+//
+// Global Variables: -
+//
+// Side Effects    : Memory operations
+//
+/----------------------------------------------------------------------*/
+
+FormulaSet_p FormulaSetAlloc()
+{
+   FormulaSet_p set = FormulaSetCellAlloc();
+
+   set->members = 0;
+   set->anchor  = WFormulaCellAlloc();
+   set->anchor->succ = set->anchor;
+   set->anchor->pred = set->anchor;
+   
+   return set;
+}
+
+/*-----------------------------------------------------------------------
+//
+// Function: FormulaSetInsert()
+//
+//   Insert new into set.
+//
+// Global Variables: -
+//
+// Side Effects    : -
+//
+/----------------------------------------------------------------------*/
+
+void FormulaSetInsert(FormulaSet_p set, WFormula_p new)
+{
+   assert(set);
+   assert(new);
+   assert(!new->set);
+   
+   new->succ = set->anchor;
+   new->pred = set->anchor->pred;
+   set->anchor->pred->succ = new;
+   set->anchor->pred = new;
+   new->set = set;
+   set->members++;
+}
+
+/*-----------------------------------------------------------------------
+//
+// Function: FormulaSetExtractEntry()
+//
+//   Extract a given formula from a formula set and return it.
+//
+// Global Variables: -
+//
+// Side Effects    : -
+//
+/----------------------------------------------------------------------*/
+
+WFormula_p FormulaSetExtractEntry(WFormula_p form)
+{
+   assert(form);
+   assert(form->set);
+
+   form->pred->succ = form->succ;
+   form->succ->pred = form->pred;
+   form->set->members--;
+   form->set = NULL;
+   form->succ = NULL;
+   form->pred = NULL;
+
+   return form;
+}
+
+WFormula_p FormulaSetExtractFirst(FormulaSet_p set);
+void FormulaSetDeleteEntry(WFormula_p form);
+
+
+/*---------------------------------------------------------------------*/
+/*                        End of File                                  */
+/*---------------------------------------------------------------------*/
+
+
---
+++ CLAUSES/ccl_formula_wrapper.h
@@ -0,0 +1,124 @@
+/*-----------------------------------------------------------------------
+
+File  : ccl_formula_wrapper.h
+
+Author: Stephan Schulz
+
+Contents
+ 
+  Data type wrapping formulas, with all the stuff that really only
+  applies to input or top-level formulae, not to recursive
+  subformulae. Also has formula sets (well, wrapped formula sets). 
+
+  Copyright 1998-2003 by the author.
+  This code is released under the GNU General Public Licence.
+  See the file COPYING in the main CLIB directory for details.
+  Run "eprover -h" for contact information.
+
+Changes
+
+<1> Wed Nov 12 17:17:44 GMT 2003
+    New
+
+-----------------------------------------------------------------------*/
+
+#ifndef CCL_FORMULA_WRAPPER
+
+#define CCL_FORMULA_WRAPPER
+
+#include <ccl_formulae.h>
+
+/*---------------------------------------------------------------------*/
+/*                    Data type declarations                           */
+/*---------------------------------------------------------------------*/
+
+typedef enum
+{
+   WPIgnoreProps    = 0,        /* For masking properties out */
+   WPInitial        = 1,        /* Input formula */
+   WPTypeAxiom      = 128,      /* Value for consistency with
+                                 * ClauseProps, we also use it for as
+                                 * a catch-all type for TPTP/TSTP
+                                 * types not explicitely checked
+                                 * for. */
+   WPTypeHypothesis = 256,      
+   WPTypeConjecture = 512,
+   WPTypeMask       = WPTypeAxiom|WPTypeHypothesis|WPTypeConjecture,
+}WFormulaProperties;
+
+
+typedef struct wformula_cell
+{ 
+   WFormulaProperties    properties;
+   long                  ident;
+   char                  *ext_ident;
+   Formula_p             formula;
+   struct formula_set_cell* set;         /* Is the formula in a set? */
+   struct wformula_cell* pred;        /* For fomula sets = doubly  */
+   struct wformula_cell* succ;        /* linked lists */
+}WFormulaCell, *WFormula_p;
+
+
+typedef struct formula_set_cell
+{
+   WFormula_p anchor;
+   long       members;
+}FormulaSetCell, *FormulaSet_p;
+
+
+/*---------------------------------------------------------------------*/
+/*                Exported Functions and Variables                     */
+/*---------------------------------------------------------------------*/
+
+extern long FormulaIdentCounter;
+
+#define FormulaSetProp(form, prop) SetProp((form), (prop))
+#define FormulaDelProp(form, prop) DelProp((form), (prop))
+#define FormulaGiveProps(form, prop) GiveProps((form), (prop))
+
+/* Are _all_ properties in prop set in form? */
+#define FormulaQueryProp(form, prop) QueryProp((form), (prop))
+/* Are any properties in prop set in form? */
+#define FormulaIsAnyPropSet(form, prop) IsAnyPropSet((form), (prop))
+#define FormulaSetType(form, type) \
+    FormulaDelProp((form),WPTypeMask);\
+    FormulaSetProp((form), (type))
+#define FormulaQueryType(form) ((form)->properties&CPTypeMask)
+
+#define WFormulaCellAlloc()    (WFormulaCell*)SizeMalloc(sizeof(WFormulaCell))
+#define WFormulaCellFree(junk) SizeFree(junk, sizeof(WFormulaCell))
+
+WFormula_p DefaultWFormulaAlloc();
+WFormula_p WFormulaAlloc(Formula_p formula);
+void       WFormulaFree(WFormula_p form);
+
+WFormula_p WFormulaTPTPParse(Scanner_p in, TB_p terms);
+void       WFormulaTPTPPrint(FILE* out, WFormula_p form, bool fullterms);
+
+WFormula_p WFormulaTSTPParse(Scanner_p in, TB_p terms);
+void       WFormulaTSTPPrint(FILE* out, WFormula_p form, bool fullterms,
+			     bool complete);
+
+
+#define FormulaSetCellAlloc()    (FormulaSetCell*)SizeMalloc(sizeof(FormulaSetCell))
+#define FormulaSetCellFree(junk) SizeFree(junk, sizeof(FormulaSetCell))
+
+FormulaSet_p FormulaSetAlloc();
+void         FormulaSetInsert(FormulaSet_p set, WFormula_p new);
+WFormula_p   FormulaSetExtractEntry(WFormula_p form);
+#define      FormulaSetEmpty(set)\
+             ((set)->anchor->succ == (set)->anchor)
+WFormula_p   FormulaSetExtractFirst(FormulaSet_p set);
+void         FormulaSetDeleteEntry(WFormula_p form);
+
+
+#endif
+
+/*---------------------------------------------------------------------*/
+/*                        End of File                                  */
+/*---------------------------------------------------------------------*/
+
+
+
+
+
