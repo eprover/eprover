c71636bf211634c6718a273ddd9a90ad9ff4732d
schulz <schulz>
2004-01-23 14:30:04 +0000

    Upped version number.

--- PROVER/eprover.c
+++ PROVER/eprover.c
@@ -32,7 +32,7 @@ Changes
 /*                  Data types                                         */
 /*---------------------------------------------------------------------*/
 
-#define VERSION      "0.81dev001"
+#define VERSION      "0.81dev002"
 #define NAME         "eprover"
 
 #ifdef SAFELOGIC
