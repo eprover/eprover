abe33568d81e8fad0422e917715f233470c25399
schulz <schulz>
2004-02-23 08:11:45 +0000

    Fixed syntax problem due to change of StrTree interface. Upped version number.

--- PROVER/eprover.c
+++ PROVER/eprover.c
@@ -32,7 +32,7 @@ Changes
 /*                  Data types                                         */
 /*---------------------------------------------------------------------*/
 
-#define VERSION      "0.81dev005"
+#define VERSION      "0.81dev006"
 #define NAME         "eprover"
 
 #ifdef SAFELOGIC
