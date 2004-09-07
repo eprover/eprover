35e875474da69da25ba16f88a9a656a37180ee4b
schulz <schulz>
2004-09-07 10:28:46 +0000

    Upped version number.

--- PROVER/eprover.c
+++ PROVER/eprover.c
@@ -32,7 +32,7 @@ Changes
 /*                  Data types                                         */
 /*---------------------------------------------------------------------*/
 
-#define VERSION      "0.82dev001"
+#define VERSION      "0.82dev002"
 #define NAME         "eprover"
 
 #define NICKNAME     "Lung Ching"
