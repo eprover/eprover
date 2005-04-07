811f08c250268e73cecb6a018fca291d0cf33f95
schulz <schulz>
2005-04-07 11:05:08 +0000

    Version number.

--- PROVER/eprover.c
+++ PROVER/eprover.c
@@ -32,8 +32,8 @@ Changes
 /*                  Data types                                         */
 /*---------------------------------------------------------------------*/
 
-/*  cvs tag E-0-82dev050 */
-#define VERSION      "0.82dev050"
+/*  cvs tag E-0-82dev051 */
+#define VERSION      "0.82dev051"
 #define NAME         "eprover"
 
 #define NICKNAME     "Lung Ching"
