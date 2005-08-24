642e2e600d2dc33b08dd3c1e571e5f58eaba0501
schulz <schulz>
2005-08-24 22:03:01 +0000

    Fixed name handing.

--- PROVER/ekb_ginsert.c
+++ PROVER/ekb_ginsert.c
@@ -129,7 +129,8 @@ int main(int argc, char* argv[])
    
    if(!ex_name && state->argv[0] && (strcmp(state->argv[0], "-")!= 0))
    {
-      ex_name = state->argv[0];
+      ex_name = FileBaseName(state->argv[0]);
+      printf("ex_name = %s\n", state->argv[0]);
    }   
    if(state->argc ==  0)
    {
