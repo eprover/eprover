534fe7331803edbc3d0e87c182a74dd490a6eef3
schulz <schulz>
2006-04-13 22:18:50 +0000

    Update.

--- development_tools/classify_problems.sh
+++ development_tools/classify_problems.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-cat /dev/null > ../problem_features
-cat /dev/null > ../problem_features_npp
+cat /dev/null > ../problem_features_tcnf16_new
 
 for file in `ls|grep \.p`; do
     echo $file
     ulimit -t 100
-    classify_problem --tstp-in -caaaaaaaaaaaa --ax-some-limit=20 --ax-many-limit=100 --lit-some-limit=15 --lit-many-limit=100 --term-medium-limit=60 --term-large-limit=1000 $file>> ../problem_features
-    classify_problem --tstp-in -caaaaaaaaaaaa --ax-some-limit=20 --ax-many-limit=100 --lit-some-limit=15 --lit-many-limit=100 --term-medium-limit=60 --term-large-limit=1000 $file >> ../problem_features_npp
+    classify_problem --tstp-in -caaaaaaaaaaaaa $file>> ../problem_features
 done
