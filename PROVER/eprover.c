9aaa6e3479ce6d195891dd1e5daacd57fff3d5ae
schulz <schulz>
2006-05-19 07:36:25 +0000

    Updated version number

--- PROVER/eprover.c
+++ PROVER/eprover.c
@@ -32,8 +32,8 @@ Changes
 /*                  Data types                                         */
 /*---------------------------------------------------------------------*/
 
-/*  cvs tag E-0-91dev001 */
-#define VERSION      "0.91dev001"
+/*  cvs tag E-0-91dev002 */
+#define VERSION      "0.91dev002"
 #define NAME         "eprover"
 
 #define NICKNAME     "Kanyam"
