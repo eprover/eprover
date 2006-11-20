bb3281f931e9d2d675d04b35121c10005f7792c0
schulz <schulz>
2006-11-20 22:09:48 +0000

    Version number.

--- PROVER/eprover.c
+++ PROVER/eprover.c
@@ -32,8 +32,8 @@ Changes
 /*                  Data types                                         */
 /*---------------------------------------------------------------------*/
 
-/*  cvs tag E-0-99dev005 */
-#define VERSION      "0.99dev005"
+/*  cvs tag E-0-99dev006 */
+#define VERSION      "0.99dev006"
 #define NAME         "eprover"
 
 #define NICKNAME     "Singtom"
