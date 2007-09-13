d0920cc91a53f561f1ef78721c5d1573bb1162e5
schulz <schulz>
2007-09-13 00:11:52 +0000

    * New version number.

--- PROVER/eprover.c
+++ PROVER/eprover.c
@@ -32,8 +32,8 @@ Changes
 /*                  Data types                                         */
 /*---------------------------------------------------------------------*/
 
-/*  cvs tag E-0-999pre004 */
-#define VERSION      "0.999pre004"
+/*  cvs tag E-0-999pre005 */
+#define VERSION      "0.999pre005"
 #define NAME         "eprover"
 
 #define NICKNAME     "Longview"
