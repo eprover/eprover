ac222a01dfb7698491ae7b7b9be213acdec65405
schulz <schulz>
2009-02-24 00:12:33 +0000

    * Doc update

--- PROVER/e_version.h
+++ PROVER/e_version.h
@@ -37,8 +37,8 @@ Changes
 /*                Exported Functions and Variables                     */
 /*---------------------------------------------------------------------*/
 
-/*  cvs tag E-1-0-004 */
-#define VERSION      "1.0-004"
+/*  cvs tag E-1-0-005 */
+#define VERSION      "1.0-005"
 
 #define E_NICKNAME     "Temi"
 
