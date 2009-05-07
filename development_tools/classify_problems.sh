f724aa0f572cf6f7cdc07c0a91d3ab1077b3aa71
schulz <schulz>
2009-05-07 09:18:15 +0000

    - Used new eq-unfolding flexibility.

--- development_tools/classify_problems.sh
+++ development_tools/classify_problems.sh
@@ -5,5 +5,5 @@ cat /dev/null > ../problem_features_tptp_3.5.0
 for file in `ls|grep \.p`; do
     echo $file
     ulimit -t 300
-    classify_problem --tstp-in --no-eq-unfolding -caaaaaaaaaaaaa $file>> ../problem_features_tptp_3.5.0
+    classify_problem --tstp-in -caaaaaaaaaaaaa $file>> ../problem_features_tptp_3.5.0
 done
