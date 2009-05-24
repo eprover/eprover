6053ade8626a63ffd4a387fadaa7d0bd3913c619
schulz <schulz>
2009-05-24 22:32:49 +0000

    Version number

--- PROVER/e_version.h
+++ PROVER/e_version.h
@@ -37,8 +37,8 @@ Changes
 /*                Exported Functions and Variables                     */
 /*---------------------------------------------------------------------*/
 
-/*  cvs tag E-1-0-013 */
-#define VERSION      "1.0-013"
+/*  cvs tag E-1-0-014 */
+#define VERSION      "1.0-014"
 
 #define E_NICKNAME     "Temi"
 
