593bef7b9322b126e65cab496f8c66ff71d7e6bc
schulz <schulz>
2009-07-20 01:07:13 +0000

    Tweaks

--- development_tools/geauswert.sh
+++ development_tools/geauswert.sh
@@ -2,11 +2,11 @@
 
 if [ "$1" ]; then
     for file in $* ; do
-	printf "%-51s%s\n" $file "`eauswert.awk $file|cut -d\: -f2-`"
+	printf "%-52s%s\n" $file "`eauswert.awk $file|cut -d\: -f2-`"
     done
 else
     for file in proto* ; do
-	printf "%-51s%s\n" $file "`eauswert.awk $file|cut -d\: -f2-`"
+	printf "%-52s%s\n" $file "`eauswert.awk $file|cut -d\: -f2-`"
     done
  fi
 
