701ee23161dbeabec120575dc0c5203544dec01e
schulz <schulz>
2009-08-24 22:26:46 +0000

    Clean-up for 1.1 release

--- PROVER/e_version.h
+++ PROVER/e_version.h
@@ -37,8 +37,8 @@ Changes
 /*                Exported Functions and Variables                     */
 /*---------------------------------------------------------------------*/
 
-/*  cvs tag E-1-1-pre13 */
-#define VERSION      "1.1-pre13"
+/*  cvs tag E-1-1-pre14 */
+#define VERSION      "1.1-pre14"
 
 #define E_NICKNAME     "Balasun"
 
