15ddceedd0238d96e07e89eb9dee210230d32a2a
schulz <schulz>
2010-05-28 22:16:33 +0000

    Version number

--- PROVER/e_version.h
+++ PROVER/e_version.h
@@ -37,8 +37,8 @@ Changes
 /*                Exported Functions and Variables                     */
 /*---------------------------------------------------------------------*/
 
-/*  cvs tag E-1-1-015*/
-#define VERSION      "1.1-015"
+/*  cvs tag E-1-1-016*/
+#define VERSION      "1.1-016"
 
 #define E_NICKNAME     "Balasun"
 
