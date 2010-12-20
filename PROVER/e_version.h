91e60876f306779418808b1f072e2c7d85226501
schulz <schulz>
2010-12-20 00:08:27 +0000

    Version number

--- PROVER/e_version.h
+++ PROVER/e_version.h
@@ -38,8 +38,8 @@ Changes
 /*                Exported Functions and Variables                     */
 /*---------------------------------------------------------------------*/
 
-/*  cvs tag E-1-2-003*/
-#define VERSION      "1.2-003"
+/*  cvs tag E-1-2-004*/
+#define VERSION      "1.2-004"
 
 #define E_NICKNAME     "Badamtam"
 
