9c3db4f4984f50a4fc117b75a6ed2eb90b600e87
schulz <schulz>
2010-12-25 19:56:04 +0000

    Cosmetics, version

--- PROVER/e_version.h
+++ PROVER/e_version.h
@@ -38,7 +38,7 @@ Changes
 /*                Exported Functions and Variables                     */
 /*---------------------------------------------------------------------*/
 
-/*  cvs tag E-1-2-006*/
+/* cvs tag E-1-2-006 */
 #define VERSION      "1.2-006"
 
 #define E_NICKNAME     "Badamtam"
