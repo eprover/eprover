05c6c9d7fc83f63b8732d5415bb376bf1636ff96
schulz <schulz>
2011-08-19 23:28:10 +0000

    Final cleanup for 1.4

--- PROVER/e_version.h
+++ PROVER/e_version.h
@@ -38,8 +38,8 @@ Changes
 /*                Exported Functions and Variables                     */
 /*---------------------------------------------------------------------*/
 
-/* cvs tag E-1-4pre011 */
-#define VERSION      "1.4pre011"
+/* cvs tag E-1-4 */
+#define VERSION      "1.4"
 
 #define E_NICKNAME     "Namring"
 
