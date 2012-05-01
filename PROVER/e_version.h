d0961721eb68dc4eb69396497a6e029c28e01c63
schulz <schulz>
2012-05-01 20:42:58 +0000

    Version number

--- PROVER/e_version.h
+++ PROVER/e_version.h
@@ -38,7 +38,7 @@ Changes
 /*                Exported Functions and Variables                     */
 /*---------------------------------------------------------------------*/
 
-/* cvs tag E-1-5pre001 */
+/* cvs tag E-1-5pre002 */
 #define VERSION      "1.5pre002"
 
 #define E_NICKNAME     "Pussimbing"
