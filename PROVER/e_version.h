e80ed5f89bdab0f18ed59042ea6b9b10f5ff9536
schulz <schulz>
2012-06-05 22:58:30 +0000

    Version number.

--- PROVER/e_version.h
+++ PROVER/e_version.h
@@ -38,8 +38,8 @@ Changes
 /*                Exported Functions and Variables                     */
 /*---------------------------------------------------------------------*/
 
-/* cvs tag E-1-6pre003 */
-#define VERSION      "1.6pre003"
+/* cvs tag E-1-6pre004 */
+#define VERSION      "1.6pre004"
 
 #define E_NICKNAME     "Tiger Hill"
 
