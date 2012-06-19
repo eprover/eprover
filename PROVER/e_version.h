f6de8b890a6b9b850521943fd591524343504bbd
schulz <schulz>
2012-06-19 21:55:46 +0000

    Updated auto mode

--- PROVER/e_version.h
+++ PROVER/e_version.h
@@ -38,8 +38,8 @@ Changes
 /*                Exported Functions and Variables                     */
 /*---------------------------------------------------------------------*/
 
-/* cvs tag E-1-6pre010 */
-#define VERSION      "1.6pre010"
+/* cvs tag E-1-6pre011 */
+#define VERSION      "1.6pre011"
 
 #define E_NICKNAME     "Tiger Hill"
 
