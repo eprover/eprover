4a1a24e4fd7fc7fedc3fa5c7a0608cccccbc6637
Stephan Schulz <schulz@eprover.org>
2012-11-03 10:17:05 +0100

    Pegasus job to classify all TPTP problems

--- development_tools/classify_problems.sh
+++ development_tools/classify_problems.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-cat /dev/null > ../problem_features_5.3.0_raw
+cat /dev/null > ../problem_features_5.4.0_raw
 
 for file in `ls|grep \.p`; do
     echo $file
     ulimit -t 500
-    classify_problem -r --tstp-in $file>> ../problem_features_5.3.0_raw
+    classify_problem -r --tstp-in $file>> ../problem_features_5.4.0_raw
 
 done
