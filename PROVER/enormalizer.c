b1fb97dada826f20b2a3107669d9fea10384ebbe
Stephan Schulz <schulz@eprover.org>
2013-02-05 23:03:47 +0100

    Added support for enormalizer, a program that applies a set of directed equations for simplifying terms. Involves some minor cleanup elsewhere.

---
+++ PROVER/enormalizer.c
@@ -0,0 +1,557 @@
+/*-----------------------------------------------------------------------
+
+File  : erewriter.c
+
+Author: Stephan Schulz
+
+Contents
+
+  Read a set of unit clauses (and/or formulas) and a set of terms. The
+  clauses/formulas are interpreted as rewrite rules. The terms are
+  normalized using these rewrite rules. If the rule system is not
+  confluent, the results are deterministic but unspecified. If the
+  rule system is not terminating, rewriting might get stuck into an
+  infinite loop.
+
+  Copyright 2013 by the author.
+  This code is released under the GNU General Public Licence and
+  the GNU Lesser General Public License.
+  See the file COPYING in the main E directory for details..
+  Run "eprover -h" for contact information.
+
+Changes
+
+<1> Mon Feb  4 23:21:45 CET 2013
+    New
+
+-----------------------------------------------------------------------*/
+
+#include <cio_commandline.h>
+#include <cio_output.h>
+#include <cio_signals.h>
+#include <ccl_splitting.h>
+#include <ccl_grounding.h>
+#include <che_clausesetfeatures.h>
+#include <ccl_formulafunc.h>
+#include <e_version.h>
+
+
+
+/*---------------------------------------------------------------------*/
+/*                  Data types                                         */
+/*---------------------------------------------------------------------*/
+
+#define NAME "enormalizer"
+
+typedef enum
+{
+   OPT_NOOPT=0,
+   OPT_HELP,
+   OPT_VERSION,
+   OPT_VERBOSE,
+   OPT_OUTPUT,
+   OPT_SILENT,
+   OPT_OUTPUTLEVEL,
+   OPT_PRINT_STATISTICS,
+   OPT_RUSAGE_INFO,
+   OPT_TPTP_PARSE,
+   OPT_TPTP_PRINT,
+   OPT_TPTP_FORMAT,
+   OPT_TSTP_PARSE,
+   OPT_TSTP_PRINT,
+   OPT_TSTP_FORMAT,
+   OPT_MEM_LIMIT,
+   OPT_CPU_LIMIT,
+   OPT_SOFTCPU_LIMIT
+}OptionCodes;
+
+
+
+/*---------------------------------------------------------------------*/
+/*                        Global Variables                             */
+/*---------------------------------------------------------------------*/
+
+OptCell opts[] =
+{
+   {OPT_HELP, 
+    'h', "help", 
+    NoArg, NULL,
+    "Print a short description of program usage and options."},
+
+   {OPT_VERSION,
+    '\0', "version",
+    NoArg, NULL,
+    "Print the version number of the program."},
+
+   {OPT_VERBOSE, 
+    'v', "verbose", 
+    OptArg, "1",
+    "Verbose comments on the progress of the program by printing "
+    "technical information to stderr."},
+
+   {OPT_OUTPUT,
+    'o', "output-file",
+    ReqArg, NULL,
+   "Redirect output into the named file."},
+
+   {OPT_SILENT,
+    's', "silent",
+    NoArg, NULL,
+    "Equivalent to --output-level=0."},
+
+   {OPT_OUTPUTLEVEL,
+    'l', "output-level",
+    ReqArg, NULL,
+    "Select an output level, greater values imply more verbose "
+    "output. Level 0 produces "
+    "nearly no output except for the final clauses, level 1 produces"
+    " minimal additional output. Higher levels are without meaning in"
+    " " NAME " (I think)."},
+ 
+   {OPT_PRINT_STATISTICS,
+    '\0', "print-statistics",
+    NoArg, NULL,
+    "Print a short statistical summary of clauses read and "
+    "generated."},
+
+   {OPT_RUSAGE_INFO,
+    'R', "resources-info",
+    NoArg, NULL,
+    "Give some information about the resources used by the system. "
+    "You will usually get CPU time information. On systems returning "
+    "more information with the rusage() system call, you will also "
+    "get information about memory consumption."},
+
+   {OPT_TPTP_PARSE,
+    '\0', "tptp-in",
+    NoArg, NULL,
+    "Parse TPTP-2 format instead of E-LOP (except includes, "
+    "which are handles as in TPTP-3, as TPTP-2 include syntax"
+    " is considered harmful)."},
+
+   {OPT_TPTP_PRINT,
+    '\0', "tptp-out",
+    NoArg, NULL,
+    "Print TPTP-2 format instead of E-LOP."},
+
+   {OPT_TPTP_FORMAT,
+    '\0', "tptp-format",
+    NoArg, NULL,
+    "Equivalent to --tptp-in and --tptp-out."},
+
+   {OPT_TPTP_PARSE,
+    '\0', "tptp2-in",
+    NoArg, NULL,
+    "Synonymous with --tptp-in."},
+
+   {OPT_TPTP_PRINT,
+    '\0', "tptp2-out",
+    NoArg, NULL,
+    "Synonymous with --tptp-out."},
+
+   {OPT_TPTP_FORMAT,
+    '\0', "tptp2-format",
+    NoArg, NULL,
+    "Synonymous with --tptp-format."},
+
+   {OPT_TSTP_PARSE,
+    '\0', "tstp-in",
+    NoArg, NULL,
+    "Parse TPTP-3 format instead of E-LOP (Note that TPTP-3 syntax "
+    "is still under development, and the version implemented may not be "
+    "fully conformant at all times. It works on all TPTP 3.0.1 input "
+    "files (including includes)."},
+   
+   {OPT_TSTP_PRINT,
+    '\0', "tstp-out",
+    NoArg, NULL,
+    "Print output clauses in TPTP-3 syntax."},
+
+   {OPT_TSTP_FORMAT,
+    '\0', "tstp-format",
+    NoArg, NULL,
+    "Equivalent to --tstp-in and --tstp-out."},
+
+   {OPT_TSTP_PARSE,
+    '\0', "tptp3-in",
+    NoArg, NULL,
+    "Synonymous with --tstp-in."},
+
+   {OPT_TSTP_PRINT,
+    '\0', "tptp3-out",
+    NoArg, NULL,
+    "Synonymous with --tstp-out."},
+
+   {OPT_TSTP_FORMAT,
+    '\0', "tptp3-format",
+    NoArg, NULL,
+    "Synonymous with --tstp-format."},
+
+   {OPT_MEM_LIMIT,
+    'm', "memory-limit",
+    ReqArg, NULL,
+    "Limit the memory the system may use. The argument is "
+    "the allowed amount of memory in MB. This option may not work "
+    "everywhere, due to broken and/or strange behaviour of setrlimit() "
+    "in some UNIX implementations. It does work under all tested "
+    "versions of Solaris and GNU/Linux."},   
+
+   {OPT_CPU_LIMIT,
+    '\0', "cpu-limit",
+    OptArg, "300",
+    "Limit the cpu time the program should run. The optional argument "
+    "is the CPU time in seconds. The program will terminate immediately"
+    " after reaching the time limit, regardless of internal state. This"
+    " option may not work " 
+    "everywhere, due to broken and/or strange behaviour of setrlimit() "
+    "in some UNIX implementations. It does work under all tested "
+    "versions of Solaris, HP-UX and GNU/Linux. As a side effect, this "
+    "option will inhibit core file writing."}, 
+
+   {OPT_SOFTCPU_LIMIT,
+    '\0', "soft-cpu-limit",
+    OptArg, "310",
+    "Limit the cpu time spend in grounding. After the time expires,"
+    " the prover will print an partial system."},  
+
+    {OPT_NOOPT,
+    '\0', NULL,
+    NoArg, NULL,
+    NULL}
+};
+
+char   *outname = NULL;
+IOFormat parse_format = LOPFormat;
+bool   print_statistics = false,
+       print_rusage = false,
+       print_result = true;
+long   give_up = 0,
+       initial_literals = 0,
+       initial_clauses = 0;
+
+/*---------------------------------------------------------------------*/
+/*                      Forward Declarations                           */
+/*---------------------------------------------------------------------*/
+
+CLState_p process_options(int argc, char* argv[]);
+void print_help(FILE* out);
+
+/*---------------------------------------------------------------------*/
+/*                         Internal Functions                          */
+/*---------------------------------------------------------------------*/
+
+int main(int argc, char* argv[])
+{
+   TB_p            terms;
+   GCAdmin_p       collector;
+   VarBank_p       freshvars;
+   Sig_p           sig;
+   ClauseSet_p     clauses;
+   FormulaSet_p    formulas;
+   Scanner_p       in;    
+   int             i;
+   CLState_p       state;
+   DefStore_p      def_store;
+   //long            symbols = 100; /* Temporary fix */
+   StrTree_p       skip_includes = NULL;
+
+   assert(argv[0]);
+   
+   InitIO(NAME);
+#ifdef STACK_SIZE
+   IncreaseMaxStackSize(argv, STACK_SIZE);
+#endif   
+   ESignalSetup(SIGXCPU);
+
+   state = process_options(argc, argv);
+   
+   OpenGlobalOut(outname);
+   
+   if(state->argc ==  0)
+   {
+      CLStateInsertArg(state, "-");
+   }
+   
+   sig       = SigAlloc(); 
+   SigInsertInternalCodes(sig);
+   terms     = TBAlloc(sig);
+   collector = GCAdminAlloc(terms);
+   clauses   = ClauseSetAlloc();
+   formulas  = FormulaSetAlloc();
+   
+   GCRegisterClauseSet(collector, clauses);
+   GCRegisterFormulaSet(collector, formulas);
+
+   for(i=0; state->argv[i]; i++)
+   {
+      in = CreateScanner(StreamTypeFile, state->argv[i], true, NULL);
+      ScannerSetFormat(in, parse_format);
+      /* ClauseSetParseList(in, clauses, terms); */
+      FormulaAndClauseSetParse(in,clauses, formulas, terms, 
+         NULL, &skip_includes);
+      CheckInpTok(in, NoToken);
+      DestroyScanner(in);
+   }
+   CLStateFree(state);
+
+   if(FormulaSetPreprocConjectures(formulas, false, false))
+   {
+      VERBOUT("Negated conjectures.\n");
+   }
+   freshvars = VarBankAlloc();
+   if(FormulaSetCNF(formulas, clauses, terms, freshvars))
+   {
+      VERBOUT("CNFization done\n");
+   }
+   VarBankFree(freshvars);
+
+   FormulaSetFree(formulas);
+
+#ifndef FAST_EXIT
+   ClauseSetFree(clauses);  
+   GCAdminFree(collector);
+
+   terms->sig = NULL;
+   TBFree(terms);
+   SigFree(sig);
+#endif
+   if(print_rusage)
+   {
+      PrintRusage(GlobalOut);
+   }
+
+   fflush(GlobalOut);
+   OutClose(GlobalOut);
+   ExitIO();
+   
+#ifdef CLB_MEMORY_DEBUG
+   MemFlushFreeList();
+   MemDebugPrintStats(stdout);
+#endif
+   
+   return 0;
+}
+
+
+/*-----------------------------------------------------------------------
+//
+// Function: process_options()
+//
+//   Read and process the command line option, return (the pointer to)
+//   a CLState object containing the remaining arguments.
+//
+// Global Variables: opts, Verbose, TermPrologArgs,
+//
+// Side Effects    : Sets variables, may terminate with program
+//                   description if option -h or --help was present
+//
+/----------------------------------------------------------------------*/
+
+CLState_p process_options(int argc, char* argv[])
+{
+   Opt_p handle;
+   CLState_p state;
+   char*  arg;
+   struct rlimit limit = {RLIM_INFINITY, RLIM_INFINITY};
+   rlim_t mem_limit = 0;
+   
+   state = CLStateAlloc(argc,argv);
+   
+   while((handle = CLStateGetOpt(state, &arg, opts)))
+   {
+      switch(handle->option_code)
+      {
+      case OPT_VERBOSE:
+	    Verbose = CLStateGetIntArg(handle, arg);
+	    break;
+      case OPT_HELP: 
+	    print_help(stdout);
+	    exit(NO_ERROR);
+	    break;
+      case OPT_VERSION:
+	    printf(NAME" " VERSION "\n");
+	    exit(NO_ERROR);
+	    break;
+      case OPT_OUTPUT:
+	    outname = arg;
+	    break;
+      case OPT_SILENT:
+	    OutputLevel = 0;
+	    break;
+      case OPT_OUTPUTLEVEL:
+	    OutputLevel = CLStateGetIntArg(handle, arg);
+	    break;
+      case OPT_PRINT_STATISTICS:
+	    print_statistics = true;
+	    break;
+      case OPT_RUSAGE_INFO:
+	    print_rusage = true;
+	    break;
+      case OPT_TPTP_PARSE:
+	    parse_format = TPTPFormat;
+	    break;
+      case OPT_TPTP_PRINT:
+	    OutputFormat = TPTPFormat;
+	    EqnFullEquationalRep = false;
+	    EqnUseInfix = false;
+	    break;
+      case OPT_TPTP_FORMAT:
+	    parse_format = TPTPFormat;	    
+	    OutputFormat = TPTPFormat;
+	    EqnFullEquationalRep = false;
+	    EqnUseInfix = false;
+	    break;
+      case OPT_TSTP_PARSE:
+            parse_format = TSTPFormat;
+            break;
+      case OPT_TSTP_PRINT:
+            OutputFormat = TSTPFormat;
+            EqnUseInfix = true;
+            break;
+      case OPT_TSTP_FORMAT:
+            parse_format = TSTPFormat;
+            OutputFormat = TSTPFormat;
+            EqnUseInfix = true;
+            break;
+      case OPT_MEM_LIMIT:
+            if(strcmp(arg, "Auto")==0)
+            {              
+               long tmpmem =  GetSystemPhysMemory();
+               long mem_limit = 0.8*tmpmem;
+               
+               if(tmpmem==-1)
+               {
+                  Error("Cannot find physical memory automatically. "
+                        "Give explicit value to --memory-limit", OTHER_ERROR);
+               }               
+               mem_limit = MEGA*mem_limit;
+               VERBOSE(fprintf(stderr, 
+                               "Physical memory determined as %ld MB\n"
+                               "Memory limit set to %ld MB\n", 
+                               tmpmem, 
+                               mem_limit););
+            }
+            else
+            {
+               mem_limit = MEGA*CLStateGetIntArg(handle, arg);
+            }
+	    break;
+      case OPT_CPU_LIMIT:
+	    HardTimeLimit = CLStateGetIntArg(handle, arg);
+	    if(SoftTimeLimit != RLIM_INFINITY)
+	    {
+	       if(HardTimeLimit<=SoftTimeLimit)
+	       {
+		  Error("Hard time limit has to be larger than soft"
+			"time limit", USAGE_ERROR);
+	       }
+	    }
+	    break;
+      case OPT_SOFTCPU_LIMIT:
+	    SoftTimeLimit = CLStateGetIntArg(handle, arg);
+	    if(HardTimeLimit != RLIM_INFINITY)
+	    {
+	       if(HardTimeLimit<=SoftTimeLimit)
+	       {
+		  Error("Soft time limit has to be smaller than hard"
+			"time limit", USAGE_ERROR);
+	       }
+	    }	    
+	    break;
+      default:
+	 assert(false);
+	 break;
+      }
+   }
+   if((HardTimeLimit!=RLIM_INFINITY)||(SoftTimeLimit!=RLIM_INFINITY))
+   {
+      if(getrlimit(RLIMIT_CPU, &limit))
+      {
+	 TmpErrno = errno;
+	 SysError("Unable to get sytem cpu time limit", SYS_ERROR);
+      }
+      SystemTimeLimit = limit.rlim_max;
+      if(SoftTimeLimit!=RLIM_INFINITY)
+      {
+	 limit.rlim_max = SystemTimeLimit; /* Redundant, but clearer */
+	 limit.rlim_cur = SoftTimeLimit;
+	 TimeLimitIsSoft = true;
+      }
+      else
+      {
+	 limit.rlim_max = SystemTimeLimit;
+	 limit.rlim_cur = HardTimeLimit;
+	 TimeLimitIsSoft = false;
+      }
+      if(setrlimit(RLIMIT_CPU, &limit))
+      {
+	 TmpErrno = errno;
+	 SysError("Unable to set cpu time limit", SYS_ERROR);
+      }
+      limit.rlim_max = RLIM_INFINITY;
+      limit.rlim_cur = 0;
+      
+      if(setrlimit(RLIMIT_CORE, &limit))
+      {
+	 TmpErrno = errno;
+	 SysError("Unable to prevent core dumps", SYS_ERROR);
+      }
+   }   
+   SetMemoryLimit(mem_limit);
+   return state;
+}
+
+
+void print_help(FILE* out)
+{
+   fprintf(out, "\n\
+\n\
+"NAME " " VERSION "\n\
+\n\
+Usage: eground [options] [files]\n\
+\n\
+Read a set of unit clauses (and/or formulas) and a set of terms. The\n\
+clauses/formulas are interpreted as rewrite rules. The terms are\n\
+normalized using these rewrite rules. If the rule system is not\n\
+confluent, the results are deterministic but unspecified. If the rule\n\
+system is not terminating, rewriting might get stuck into an infinite\n\
+loop. The normalized terms are as output.\n\
+\n");
+   PrintOptions(stdout, opts, "Options\n\n");
+   fprintf(out, "\n\
+"STS_COPYRIGHT", " STS_MAIL "\n\
+\n\
+This program is a part of the support structure for the E equational\n\
+theorem prover. You can find the latest version of the E distribution\n\
+as well as additional information at\n"
+E_URL
+"\n\n\
+This program is free software; you can redistribute it and/or modify\n\
+it under the terms of the GNU General Public License as published by\n\
+the Free Software Foundation; either version 2 of the License, or\n\
+(at your option) any later version.\n\
+\n\
+This program is distributed in the hope that it will be useful,\n\
+but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n\
+GNU General Public License for more details.\n\
+\n\
+You should have received a copy of the GNU General Public License\n\
+along with this program (it should be contained in the top level\n\
+directory of the distribution in the file COPYING); if not, write to\n\
+the Free Software Foundation, Inc., 59 Temple Place, Suite 330,\n\
+Boston, MA  02111-1307 USA\n\
+\n\
+The original copyright holder can be contacted as\n\
+\n"
+STS_SNAIL
+"\n");
+
+}
+
+
+/*---------------------------------------------------------------------*/
+/*                        End of File                                  */
+/*---------------------------------------------------------------------*/
+
+
