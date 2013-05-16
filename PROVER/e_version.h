2ccdbd144214f58615fc35a974861c3b860605bd
Stephan Schulz <schulz@eprover.org>
2013-05-17 01:03:27 +0200

    Version number

--- PROVER/e_version.h
+++ PROVER/e_version.h
@@ -38,8 +38,8 @@ Changes
 /*                Exported Functions and Variables                     */
 /*---------------------------------------------------------------------*/
 
-/* git tag E-1-7dev005 */
-#define VERSION      "1.7dev005"
+/* git tag E-1-7dev006 */
+#define VERSION      "1.7dev006"
 
 #define E_NICKNAME     "Jun Chiabari"
 
