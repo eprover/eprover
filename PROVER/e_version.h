a97df3afe443f3a0dafcf1fd95fb898c207f3964
Stephan Schulz <schulz@eprover.org>
2013-08-19 23:46:26 +0200

    Version number

--- PROVER/e_version.h
+++ PROVER/e_version.h
@@ -38,8 +38,8 @@ Changes
 /*                Exported Functions and Variables                     */
 /*---------------------------------------------------------------------*/
 
-/* git tag E-1-8-001 */
-#define VERSION      "1.8-001"
+/* git tag E-1-8-002 */
+#define VERSION      "1.8-002"
 
 #define E_NICKNAME     "Gopaldhara"
 
