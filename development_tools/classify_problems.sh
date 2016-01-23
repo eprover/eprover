195996a9fbc989cf5e132256f2eccea4c7e88128
Stephan Schulz <schulz@eprover.org>
2016-01-23 16:42:05 +0100

    Update wrong or redundant runscripts.

--- development_tools/classify_problems.sh
+++ development_tools/classify_problems.sh
@@ -2,9 +2,9 @@
 
 cat /dev/null > ../problem_features_6.0.0_raw
 
-#for file in `ls|grep [+-].*\.p`; do
-for file in `cat ../missing`; do
+for file in `ls|grep [+-].*\.p`; do
+#for file in `cat ../missing`; do
     echo $file
     ulimit -t 5000
-    classify_problem --tstp-in $file>> ../problem_features_6.0.0_raw
+    classify_problem --tstp-in $file>> ../problem_features_6.3.0_raw
 done
