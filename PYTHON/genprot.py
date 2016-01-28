af38de0a33333ddfac45101d1a87218bf63e1188
Martin Möhrmann <moehrmann@eprover.org>
2016-01-28 11:16:47 +0100

    genprot.py: fixes, better failure detection and added archive name to metadata

--- PYTHON/genprot.py
+++ PYTHON/genprot.py
@@ -22,7 +22,7 @@ Options:
 --header     add cvs header with column names
 --delimiter  delimiter between values
 --default    default value for fields in cvs
---metadata   add metadata from directory structure - filename and heuristic name
+--metadata   add metadata from directory structure - filename and heuristic and jobarchive name
 --compact    do not add alignment whitespace
 --features   add feature columns with features of the problem taken from supplied feature file
 
@@ -67,31 +67,32 @@ import argparse
 import tarfile
 import zipfile
 
-from itertools import chain
+from itertools import chain, islice
 from os.path import dirname, splitext, basename, isdir, isfile
 from collections import defaultdict
 
 protfile = (lambda name: "protocol_G----_" + name + ".csv")
 
 firstkeys   = ["Problem", "Status", "User time", "Failure", "Version", "Preprocessing time"]
-metakeys    = ["Filename", "Heuristic"]
-removekeys  = ["eprover","Command", "Computer", "Model", "CPU", "Memory", "OS", "CPULimit", "DateTime", "CPUTime"]
-featurekeys = ["Type","Equational"]
+metakeys    = ["Filename", "Heuristic", "Archivename"]
+removekeys  = ["eprover", "Command", "Computer", "Model", "CPU", "Memory", "OS", "CPULimit", "DateTime", "CPUTime"]
+featurekeys = ["Type", "Equational"]
 
 failuremap = {"User resource limit exceeded"    :"maxres",
               "Out of unprocessed clauses!"     :"incomplete",
               "Resource limit exceeded (memory)":"maxmem",
               "Resource limit exceeded (time)"  :"maxtime",
-              "exec failed"                     :"starexecfail"}
+              "exec failed"                     :"starexec",
+              "unknown"                         :"unknown"}
 
-
-statusmap = {"exec failed"                  :"F",
-	         "SZS status GaveUp"            :"F",
-			 "SZS status ResourceOut"       :"F",
-			 "SZS status Satisfiable"       :"N",
-			 "SZS status CounterSatisfiable":"N",
-			 "SZS status Theorem"           :"T",
-			 "SZS status Unsatisfiable"     :"T"}
+statusmap = {"unknown"                      :"F",
+             "exec failed"                  :"F",
+             "SZS status GaveUp"            :"F",
+             "SZS status ResourceOut"       :"F",
+             "SZS status Satisfiable"       :"N",
+             "SZS status CounterSatisfiable":"N",
+             "SZS status Theorem"           :"T",
+             "SZS status Unsatisfiable"     :"T"}
 
 def rjust(amount): return lambda s: s.rjust(amount)
 def ljust(amount): return lambda s: s.ljust(amount)
@@ -105,6 +106,9 @@ adjustmap.update({"User time"         :rjust( 8),
                   "Heuristic"         :rjust(12),
                   "Filename"          :ljust(12),
                   "Problem"           :ljust( 8)})
+
+def firstvalue(dictionary):
+	return next(iter(dictionary.values()))
 
 def clean_value(value):
 	value = value.strip()
@@ -136,7 +140,7 @@ def remove_timestamp(line):
 				return ""
 
 def make_entry(lines):
-	entry = {"Failure":"success"}
+	entry = dict()
 	for line in lines:
 		line = line.decode()
 		line = remove_timestamp(line)
@@ -156,19 +160,25 @@ def make_entry(lines):
 			entry[key] = value
 	return entry
 
-def process_file(data, features, path, fileopener, info):
+def process_file(data, features, jobarchivepath, path, fileopener, info):
 	problemname   = basename(dirname(path))
 	heuristicname = "_".join(basename(dirname(dirname(path))).split("_")[-2:])
 	eversion      = basename(dirname(dirname(path))).split("_",1)[0][2:]
 	fileextension = splitext(path)[-1]
 	filename      = basename(path)
+	archivename   = basename(jobarchivepath)
 	if problemname and heuristicname and fileextension == ".txt" and (("+" in problemname) or ("-" in problemname)):
-		#print(infile+" : "+path)
 		entry = make_entry(fileopener(info).readlines())
-		entry.update({"Heuristic":heuristicname,"Filename":filename})
+		entry.update({"Heuristic":heuristicname, "Filename":filename, "Archivename":archivename})
 		#fix output error in e version 1.9.1pre005
 		if int(entry.get("Proof object given clauses",0)) > int(entry.get("Proof search given clauses",0)):
 			swap(entry,"Proof object given clauses","Proof search given clauses")
+		if "Status" not in entry:
+			entry["Status"] = statusmap["unknown"]
+			if "Failure" not in entry:
+				entry["Failure"] = failuremap["unknown"]
+		if "Failure" not in entry:
+			entry["Failure"] = "success"
 		if entry["Failure"] == failuremap["exec failed"]:
 			entry["Problem"] = problemname
 			entry["Version"] = eversion
@@ -226,17 +236,17 @@ if __name__ == "__main__":
 		if tarfile.is_tarfile(infile):
 			with tarfile.open(infile) as tfile:
 				for tinfo in tfile:
-					process_file(data, features, tinfo.name, tfile.extractfile, tinfo)
+					process_file(data, features, infile, tinfo.name, tfile.extractfile, tinfo)
 		elif zipfile.is_zipfile(infile):
 			with zipfile.ZipFile(infile) as zfile:
 				for zinfo in zfile.infolist():
-					process_file(data, features, zinfo.filename, zfile.open, zinfo)
-		elif isdir(path):
+					process_file(data, features, infile, zinfo.filename, zfile.open, zinfo)
+		elif isdir(infile):
 			for _, _, files in os.walk(path):
 				for path in files:
-					process_file(data, features, path, open, path)
+					process_file(data, features, infile, path, open, path)
 		else:
-			print("Dont know how to open %s." % infile)
+			print("Do not know how to open %s." % infile)
 
 	keys       = set(chain.from_iterable(entry.keys() for problems in data.values() for entry in problems.values()))
 	fieldnames = firstkeys + sorted(keys.difference(set(firstkeys + removekeys + metakeys + featurekeys)))
@@ -245,9 +255,9 @@ if __name__ == "__main__":
 	if args.features:
 		fieldnames += featurekeys
 	for heuristic, problems in data.items():
-		with open(protfile(heuristic), "w", newline="") as report:
-			report.write("#" + next(iter(problems.values()))["Command"] + "\n")
-			report.writelines(("#" + str(number) + " " + name + "\n" for number, name in enumerate(fieldnames, 1)))
+		with open(protfile(heuristic), "w") as report:
+			report.write("# {0[Command]} \n".format(firstvalue(problems)))
+			report.writelines("# {} {} \n".format(*pair) for pair in enumerate(fieldnames, 1) )
 			if not args.header:
 				report.write("#")
 			report.write(args.delimiter.join(fieldnames)+"\n")
