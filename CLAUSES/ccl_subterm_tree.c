d756ad6980a44b565e7f7184740b7d3c0682c0f1
Martin Möhrmann <moehrmann@eprover.org>
2016-06-04 09:49:09 +0200

    Fixed bug in FP-Indexing for TFF
    Term comparison by entry_no fails if there are different variables with the
    same number. Changed to compare term pointers.
    
    Fix extracted from commit in tff branch:
    e9a6caad4e7c22455ae24016b30ee9d4ae535c80

--- CLAUSES/ccl_subterm_tree.c
+++ CLAUSES/ccl_subterm_tree.c
@@ -183,7 +183,7 @@ void SubtermPosFree(SubtermOcc_p soc)
 //
 // Function: CmpSubtermCells()
 //
-//   Compare two SubtermOccurance cells via their term entry_no. This
+//   Compare two SubtermOccurance cells via their term pointers. This
 //   is a synthetic but machine-independent measure useful primarily
 //   for indexing.
 //
@@ -198,15 +198,7 @@ int CmpSubtermCells(const void *soc1, const void *soc2)
    const SubtermOcc_p s1 = (const SubtermOcc_p) soc1;
    const SubtermOcc_p s2 = (const SubtermOcc_p) soc2;
 
-   if(s1->term->entry_no > s2->term->entry_no)
-   {
-      return 1;
-   }
-   if(s1->term->entry_no < s2->term->entry_no)
-   {
-      return -1;
-   }
-   return 0;
+   return PCmp(s1->term, s2->term);
 }
 
 
