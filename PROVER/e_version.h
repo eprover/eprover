5cd2a88d9612889eb0dfe8c4109df81be00a42dc
Stephan Schulz <schulz@eprover.org>
2016-06-22 00:35:11 +0200

    Version number

--- PROVER/e_version.h
+++ PROVER/e_version.h
@@ -38,8 +38,8 @@ Changes
 /*                Exported Functions and Variables                     */
 /*---------------------------------------------------------------------*/
 
-/* git tag E-1-9-1pre013 */
-#define VERSION      "1.9.1pre013"
+/* git tag E-1-9-1pre014 */
+#define VERSION      "1.9.1pre014"
 
 #define E_NICKNAME     "Sungma"
 
