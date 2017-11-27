3d465414f72b2fbb928d693d47656a0c73d1e90c
Stephan Schulz <schulz@eprover.org>
2017-11-27 15:46:37 +0100

    Version number

--- PROVER/e_version.h
+++ PROVER/e_version.h
@@ -33,8 +33,8 @@ Contents
 /*                Exported Functions and Variables                     */
 /*---------------------------------------------------------------------*/
 
-/* git tag E-2.1pre005 */
-#define VERSION      "2.1pre005"
+/* git tag E-2.1pre006 */
+#define VERSION      "2.1pre006"
 
 #define E_NICKNAME     "Maharani Hills"
 
