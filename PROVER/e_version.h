3695f3464963289a0f57046ef83b032c85bd8f6d
Stephan Schulz <schulz@eprover.org>
2018-10-14 18:25:47 +0200

    Version number

--- PROVER/e_version.h
+++ PROVER/e_version.h
@@ -32,9 +32,9 @@ Contents
 /*                Exported Functions and Variables                     */
 /*---------------------------------------------------------------------*/
 
-/* git tag E-2.2pre010 */
+/* git tag E-2.2pre012 */
 
-#define PVERSION      "2.2pre011"
+#define PVERSION      "2.2pre012"
 
 #ifdef NDEBUG
 #define VERSION PVERSION
