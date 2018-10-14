361417a7883aefaf91f6be64e172e7c1b11b7d8f
Stephan Schulz <schulz@eprover.org>
2018-10-14 19:02:10 +0200

    Version number, added --free-numbers to auto runscripts

--- PROVER/e_version.h
+++ PROVER/e_version.h
@@ -32,9 +32,9 @@ Contents
 /*                Exported Functions and Variables                     */
 /*---------------------------------------------------------------------*/
 
-/* git tag E-2.2pre012 */
+/* git tag E-2.2pre013 */
 
-#define PVERSION      "2.2pre012"
+#define PVERSION      "2.2pre013"
 
 #ifdef NDEBUG
 #define VERSION PVERSION
