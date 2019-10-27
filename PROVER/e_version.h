e21bba36e48498c1ae2c1d99fe26320460595501
Stephan Schulz <schulz@eprover.org>
2019-10-27 16:34:15 +0100

    Documentation updates for E 2.4 release, version number

--- PROVER/e_version.h
+++ PROVER/e_version.h
@@ -32,9 +32,9 @@ Contents
 /*                Exported Functions and Variables                     */
 /*---------------------------------------------------------------------*/
 
-/* git tag E-2.4_pre007 */
+/* git tag E-2.4 */
 
-#define PVERSION      "2.4_pre007"
+#define PVERSION      "2.4"
 
 #include "e_gitcommit.h"
 
