ab928ddf6454e03c50ca26e326652df538bc76ff
Petar Vukmirovic <p.vukmirovic@vu.nl>
2019-12-20 17:08:54 +0100

    fix with printing clauses

--- CLAUSES/ccl_clauses.c
+++ CLAUSES/ccl_clauses.c
@@ -1419,7 +1419,7 @@ void ClauseTSTPPrint(FILE* out, Clause_p clause, bool fullterms, bool complete)
    }
    fprintf(out, "%s, ", typename);
 
-   if(is_untyped)
+   if(is_untyped && problemType != PROBLEM_HO)
    {
       ClauseTSTPCorePrint(out, clause, fullterms);
    }
