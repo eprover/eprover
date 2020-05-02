faba7a7b24adc6d9c5454505a9cad1f2725d4dfc
Stephan Schulz <schulz@eprover.org>
2020-05-02 23:43:20 +0200

    - Trivialities

--- development_tools/classify_problems.sh
+++ development_tools/classify_problems.sh
@@ -3,15 +3,15 @@
 # Delete this if only missing problems are added!
 # cat /dev/null > ../problem_features_7.2.0_raw
 
-#for file in `ls|grep [_+=-].*\.p`; do
-for file in `gawk '/^[^#]/{print $1}' ../missing`; do
+for file in `ls|grep [_+=-].*\.p`; do
+#for file in `gawk '/^[^#]/{print $1}' ../missing`; do
     echo "Processing " $file
     ProblemSPC=`grep " SPC " $file | sed -e "s/.* : //"`
     # echo $ProblemSPC
     if  [ `expr "$ProblemSPC" : "TF0.*"` != 0 ]  || [ `expr "$ProblemSPC" : "FOF.*"` != 0 ] || [ `expr "$ProblemSPC" : "CNF.*"` != 0 ]  ; then
        # echo $file
        ulimit -t 5000
-       classify_problem --free-numbers --sine=Auto $file>> ../problem_features_7.2.0_raw
+       classify_problem --free-numbers --sine=Auto --no-preprocessing $file>> ../problem_features_7.3.0_nopraw
     else
            echo "Skipping " $file
     fi
