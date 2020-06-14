ac279b72750bfe64bd79e7a40c6b4f1328647248
Stephan Schulz <schulz@eprover.org>
2020-06-14 21:00:16 +0200

    Version number

--- PROVER/e_version.h
+++ PROVER/e_version.h
@@ -32,9 +32,9 @@ Contents
 /*                Exported Functions and Variables                     */
 /*---------------------------------------------------------------------*/
 
-/* git tag E-2.4dev008 */
+/* git tag E-2.5pre001 */
 
-#define PVERSION      "2.4dev008"
+#define PVERSION      "2.5pre001"
 
 #include "e_gitcommit.h"
 
@@ -50,7 +50,7 @@ Contents
 #define VERSION PVERSION LFH "-DEBUG"
 #endif
 
-#define E_NICKNAME     "Sandakphu"
+#define E_NICKNAME     "Avongrove"
 
 #define E_URL "http://www.eprover.org"
 
