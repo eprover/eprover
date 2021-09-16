49042cac8b6d478c42d28e6bd76372b5347e78c9
Petar Vukmirovic <petar.vukmirovic2@gmail.com>
2021-09-16 19:48:43 +0200

    more

---
+++ HEURISTICS/che_new_autoschedule.h
@@ -0,0 +1,8 @@
+#include "che_hcb.h"
+
+const char* DEFAULT_MASK = "aaaaa-aaaaaa-aa--aa";
+const char* RAW_DEFAULT_MASK = "aaa----aaa";
+
+void HeuristicForRawCategory(const char* raw_category, HeuristicParms_p parms);
+void HeuristicForCategory(const char* category, HeuristicParms_p parms);
+
