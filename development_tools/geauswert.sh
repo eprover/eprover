df07d479e83b6bd67da1f54a8dc9658408f9ac09
Stephan Schulz <schulz@eprover.org>
2022-02-28 01:07:04 +0100

    Fixed script

--- development_tools/geauswert.sh
+++ development_tools/geauswert.sh
@@ -1,11 +1,20 @@
 #! /bin/sh
 
+time=""
+optflag="-t"
+
+if [ "$1" = "-t" ]; then
+    shift
+    time=$1
+    shift
+fi
+
 if [ "$1" ]; then
     for file in $* ; do
-	printf "%-65s%s\n" $file "`eauswert.awk $file|cut -d\: -f2-`"
+	printf "%-65s%s\n" $file "`eauswert.awk $file 1000000 $time |cut -d\: -f2-`"
     done
 else
     for file in proto* ; do
-	printf "%-65s%s\n" $file "`eauswert.awk $file|cut -d\: -f2-`"
+	printf "%-65s%s\n" $file "`eauswert.awk $file 1000000 $time |cut -d\: -f2-`"
     done
  fi
