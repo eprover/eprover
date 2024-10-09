6ef2b900ba74ab16c9ed170d6c8c24c4dd1d8616
Stephan Schulz <schulz@eprover.org>
2024-10-09 11:58:13 +0200

    Version number

--- PROVER/e_version.h
+++ PROVER/e_version.h
@@ -31,9 +31,9 @@ Contents
 /*                Exported Functions and Variables                     */
 /*---------------------------------------------------------------------*/
 
-/* git tag E-3.2.2 */
+/* git tag E-3.2.3 */
 
-#define PVERSION      "3.2.2"
+#define PVERSION      "3.2.3"
 
 #include "e_gitcommit.h"
 
